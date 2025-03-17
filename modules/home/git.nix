46553b8b54cea180fbed1034aa15e1f24bfbf676
jfulfo <47907450+jfulfo@users.noreply.github.com>
2025-03-17 19:36:22 -0400

    add direnv for coq and waybar battery

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -1,11 +1,16 @@
-{ host, ... }:
-let
+{host, ...}: let
   inherit (import ../../hosts/${host}/variables.nix) gitUsername gitEmail;
-in
-{
+in {
   programs.git = {
     enable = true;
     userName = "${gitUsername}";
     userEmail = "${gitEmail}";
+    ignores = [
+      ".envrc"
+      ".env"
+      ".venv"
+      ".direnv"
+      "shell.nix"
+    ];
   };
 }
--- modules/home/waybar/default.nix
+++ modules/home/waybar/default.nix
@@ -44,6 +44,7 @@ in {
           "custom/separator#dot-line"
           "tray"
           "custom/separator#dot-line"
+          "battery"
           "custom/power"
           "custom/separator#blank"
         ];
