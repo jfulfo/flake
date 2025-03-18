2ce28d2f8fa888162fa26b1ee8531cdad75f6df1
jfulfo <47907450+jfulfo@users.noreply.github.com>
2025-03-18 01:20:11 -0400

    conceal doesn't work, sad!

--- modules/home/nvim/languages/latex.nix
+++ modules/home/nvim/languages/latex.nix
@@ -5,17 +5,13 @@
       package = pkgs.vimPlugins.vimtex;
       lazy = true;
       ft = "tex";
-      setupOpts = {
-        init = ''
-          vim.g.tex_flavor = "latex"
-          vim.g.vimtex_view_method = "zathura"
-          vim.g.vimtex_quickfix_mode = 0
-          vim.g.conceallevel = 1
-          vim.g.tex_conceal = "abdmg"
-          vim.g.vimtexd_view_forward_search_on_start = false
-        '';
-      };
       after = ''
+        vim.g.tex_flavor = "latex"
+        vim.g.vimtex_view_method = "zathura"
+        vim.g.vimtex_quickfix_mode = 0
+        vim.g.conceallevel = 2
+        vim.g.tex_conceal = "abdmg"
+        vim.g.vimtexd_view_forward_search_on_start = false
         vim.api.nvim_command('unlet b:did_ftplugin')
         vim.api.nvim_command('call vimtex#init()')
       '';
