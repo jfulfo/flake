86733d096b407f8610213644d1a725f38a61c73a
jfulfo <47907450+jfulfo@users.noreply.github.com>
2025-04-08 12:06:56 -0400

    lix to 2.92.0-3

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
       flake = false;
     };
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.92.0-3.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
