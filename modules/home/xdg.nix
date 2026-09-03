71a6f7612f4c0a6e13c7c6b18fc7703742c7603c
jfulfo <47907450+jfulfo@users.noreply.github.com>
2026-09-03 12:29:07 -0500

    chore: make Projects folder lowercase

--- modules/home/xdg.nix
+++ modules/home/xdg.nix
@@ -10,6 +10,7 @@
       download = "${config.home.homeDirectory}/downloads";
       music = "${config.home.homeDirectory}/music";
       videos = "${config.home.homeDirectory}/videos";
+      projects = "${config.home.homeDirectory}/projects";
       publicShare = null;
       templates = null;
     };
